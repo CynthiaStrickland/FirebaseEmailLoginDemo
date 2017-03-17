1f13a46e2a437f33ef3d7c5db667e017da112ea9
Cynthia Strickland <cynthiastrickland001@gmail.com>
2017-03-17 12:07:52 -0700

    UX Modifications to MyHomeViewController

---
+++ FirebaseEmailLoginDemo/MyHomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MyHomeViewController.swift
+//  FirebaseEmailLoginDemo
+//
+//  Created by Cynthia Strickland on 3/17/17.
+//  Copyright © 2017 Fenix Designz. All rights reserved.
+//
+
+import UIKit
+
+class MyHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
